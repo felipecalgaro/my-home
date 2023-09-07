a58f89923f68f34656a9c194d897374b7a1f353e
Felipe <pipecalgaro@gmail.com>
2023-09-07 13:38:16 -0300

    feat: add custom screen size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
@@ -7,6 +8,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         "custom-black": "#1F1F1F",
