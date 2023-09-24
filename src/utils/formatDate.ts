44e240c0c6de2ca8812dcf23cc805bb8e5b2d9eb
Felipe <pipecalgaro@gmail.com>
2023-09-24 16:04:34 -0300

    feat: add options to formatDate function

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,19 @@
-export default function formatDate(date: Date) {
-  return new Intl.DateTimeFormat("pt-BR", {
+type Locale = "us" | "pt";
+
+export default function formatDate(
+  date: Date,
+  locale: Locale,
+  keepSlash?: boolean
+) {
+  let formattedDate = new Intl.DateTimeFormat(locale, {
     day: "2-digit",
     month: "2-digit",
     year: "2-digit",
   }).format(date);
+
+  if (!keepSlash) {
+    formattedDate = formattedDate.replaceAll("/", "-");
+  }
+
+  return formattedDate;
 }
