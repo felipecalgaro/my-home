3a3dbb6b0a800ead02fc884ab3537e9ece7cad8c
Felipe <pipecalgaro@gmail.com>
2023-09-27 13:51:26 -0300

    feat: implement search params state management

--- src/app/homes/page.tsx
+++ src/app/homes/page.tsx
@@ -1,47 +1,47 @@
-import { Card } from '@/components/Card';
 import { DateInput } from '@/components/DateInput';
+import { HomesSection } from '@/components/HomesSection';
 import { IconLabel } from '@/components/IconLabel';
-import { PaginationButton } from '@/components/PaginationButton';
 import { TextInput } from '@/components/TextInput';
-import { getHomesByAvailabilityService } from '@/services/getHomesByAvailabilityService';
 import Image from 'next/image';
+import Link from 'next/link';
 
-export default async function Homes() {
-  const homes = await getHomesByAvailabilityService({
-    availableFrom: new Date(),
-    availableUntil: new Date('2023-10-09')
-  })
+export type SearchParams = {
+  [key: string]: string | undefined
+}
+
+interface HomesProps {
+  searchParams: SearchParams
+}
+
+export default function Homes({ searchParams }: HomesProps) {
+  const { checkIn, checkOut, location } = searchParams
 
   return (
     <main className='px-4'>
       <form className='flex justify-evenly items-center flex-wrap gap-x-12 gap-y-10 mt-10'>
         <section className='flex justify-center items-center'>
-          <IconLabel htmlFor='location'>
-            <Image src='/pin.svg' alt='Pin Icon' width={20} height={20} />
-          </IconLabel>
-          <TextInput placeholder='Ex.: "Salvador, Bahia"' name="place" id="place" />
-        </section>
-        <section className='flex justify-center items-center'>
           <IconLabel htmlFor='checkIn'>
             <Image src='/calendar.svg' alt='Calendar Icon' width={24} height={24} />
           </IconLabel>
-          <DateInput placeholder='Check-in' id='checkIn' />
-          <DateInput className='ml-0.5 rounded-r-3xl' placeholder='Check-out' />
+          <DateInput placeholderText='Check-in' id='checkIn' name='checkIn' />
+          <DateInput className='ml-0.5 rounded-r-3xl' id='checkOut' placeholderText='Check-out' name='checkOut' />
         </section>
-        <button className='bg-custom-black px-12 py-2 xs:text-lg text-base text-white rounded-xl flex justify-center items-center gap-x-2 active:scale-95 transition-transform duration-200'>
+        <section className='flex justify-center items-center'>
+          <IconLabel htmlFor='location'>
+            <Image src='/pin.svg' alt='Pin Icon' width={20} height={20} />
+          </IconLabel>
+          <TextInput placeholder='Ex.: "Salvador" ou "Bahia"' name="location" id="location" />
+        </section>
+        <Link href={``} type='submit' className='bg-custom-black px-12 py-2 xs:text-lg text-base text-white rounded-xl flex justify-center items-center gap-x-2 active:scale-95 transition-transform duration-200 cursor-pointer'>
           Search
           <Image src='/search.svg' alt='Search Icon' width={20} height={20} />
-        </button>
+        </Link>
       </form>
-      <section className='py-20 md:px-16 px-4 px flex justify-center items-start flex-wrap gap-x-10 gap-y-20'>
-        {homes.map((home) => (
-          <Card id={home.id} availableFrom={home.availableFrom!} availableUntil={home.availableUntil!} imageUrl={home.image_url} location={home.location} price={home.price} key={home.id} />
-        ))}
-      </section>
-      <section className='flex justify-center items-center mb-10 gap-x-8'>
-        <PaginationButton iconUrl='/previous.svg' />
-        <PaginationButton iconUrl='/next.svg' />
-      </section>
+      <HomesSection filter={{
+        availableFrom: checkIn ? new Date(checkIn) : new Date(),
+        availableUntil: checkOut ? new Date(checkOut) : new Date(),
+        location: location || null
+      }} />
     </main>
   )
 }
