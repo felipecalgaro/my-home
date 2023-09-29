57a44c588931dd8e1a0a5abf7adb51b7b42bc518
Felipe <pipecalgaro@gmail.com>
2023-09-29 14:25:56 -0300

    fix: update page layout

--- src/app/homes/[id]/page.tsx
+++ src/app/homes/[id]/page.tsx
@@ -18,8 +18,9 @@ export default async function Home({ params }: HomeProps) {
 
   return (
     <main className='flex flex-col justify-center items-center gap-32 pb-16 px-8'>
-      <section className='flex justify-center items-center flex-wrap lg:gap-x-52 gap-x-20 gap-y-20 pt-16'>
+      <section className='flex justify-center items-center flex-wrap lg:gap-x-32 gap-x-20 gap-y-20 pt-16'>
         <Image alt='House Image' src={'/example.png'} width={400} height={400} />
+        <hr className='lg:h-80 h-px lg:w-px w-2/3 bg-[#d4d1d1]' />
         <div className='flex justify-center items-start flex-col lg:gap-12 gap-8'>
           <HomeDetail>
             <Image src='/pin.svg' alt='pin-icon' width={24} height={24} />
@@ -41,24 +42,24 @@ export default async function Home({ params }: HomeProps) {
       </section>
       <section className='flex justify-center items-center w-full lg:gap-x-64 gap-x-24 flex-wrap'>
         <form className='flex flex-col justify-center items-start gap-12'>
-          <h1 className='text-4xl font-medium'>Contact the owner</h1>
+          <h1 className='text-4xl font-medium'>Contate o proprietário</h1>
           <div className='flex flex-col justify-center items-start gap-2'>
             <label className='ml-2 text-xl font-medium' htmlFor="email">Email</label>
             <div className='flex justify-center items-center'>
               <IconLabel htmlFor='email'>
                 <Image src='/email.svg' alt='email-icon' width={24} height={24} />
               </IconLabel>
-              <TextInput placeholder='Enter your email' name='email' id='email' />
+              <TextInput placeholder='Digite seu email' name='email' id='email' />
             </div>
           </div>
           <div className='flex flex-col justify-center items-start gap-2 w-full'>
-            <label className='ml-2 text-xl font-medium' htmlFor="message">Message</label>
-            <textarea name='message' id='message' rows={8} placeholder='Enter the message' className='bg-[#D9D9D9] border-[#c8c8c8] border drop-shadow-md rounded-3xl xs:text-xl text-lg p-4 xs:w-full w-64 focus:outline-none focus:ring-1 focus:ring-slate-500 font-extralight xs:placeholder:text-lg placeholder:text-base placeholder:text-custom-gray'></textarea>
+            <label className='ml-2 text-xl font-medium' htmlFor="message">Mensagem</label>
+            <textarea name='message' id='message' rows={8} placeholder='Digite a mensagem' className='bg-[#D9D9D9] border-[#c8c8c8] border drop-shadow-md rounded-3xl xs:text-xl text-lg p-4 xs:w-full w-64 focus:outline-none focus:ring-1 focus:ring-slate-500 font-extralight xs:placeholder:text-lg placeholder:text-base placeholder:text-custom-gray'></textarea>
           </div>
-          <button className='hover:bg-custom-black border border-custom-black px-12 py-1.5 xs:text-lg text-base text-custom-black hover:text-white rounded-xl flex justify-center items-center gap-x-2 active:scale-95 transition-all duration-200'>Send</button>
+          <button className='hover:bg-custom-black border border-custom-black px-12 py-1.5 xs:text-lg text-base text-custom-black hover:text-white rounded-xl flex justify-center items-center gap-x-2 active:scale-95 transition-all duration-200'>Enviar</button>
         </form>
         <div className='h-full self-start pt-32'>
-          <h1 className='text-2xl font-semibold'>Sending an email to:</h1>
+          <h1 className='text-2xl font-semibold'>Enviando email para:</h1>
           <h1 className='text-2xl text-center text-custom-gray'>{home.ownerEmail}</h1>
         </div>
       </section>
