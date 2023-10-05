bb633862617e4e5bb865336c86806ff98e54c7bb
Felipe <pipecalgaro@gmail.com>
2023-10-05 19:44:26 -0300

    feat: add styles to IconLabel component

--- src/components/IconLabel.tsx
+++ src/components/IconLabel.tsx
@@ -7,7 +7,7 @@ interface IconLabelProps {
 
 export function IconLabel({ children, htmlFor }: IconLabelProps) {
   return (
-    <label htmlFor={htmlFor} className='bg-[#D9D9D9] xs:w-14 w-10 rounded-full rounded-r-none border-r-2 border-white text-xl h-12 flex justify-center items-center pl-2 pr-1'>
+    <label htmlFor={htmlFor} className='bg-[#D9D9D9] border-[#c8c8c8] border drop-shadow-md xs:w-14 w-10 rounded-full rounded-r-none mr-0.5 text-xl h-12 flex justify-center items-center pl-2 pr-1'>
       {children}
     </label>
   )
