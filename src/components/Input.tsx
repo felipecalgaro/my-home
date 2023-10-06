6cb52ef62928bb31b1ab3f647bcf1743888d59b0
Felipe <pipecalgaro@gmail.com>
2023-10-06 20:27:49 -0300

    fix: update Input props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,10 +9,10 @@ interface InputProps {
   placeholder: string
   label: string
   children?: ReactNode
-  value?: string
+  defaultValue?: string
 }
 
-export function Input({ id, name, type, className, placeholder, label, children, value }: InputProps) {
+export function Input({ id, name, type, className, placeholder, label, children, defaultValue }: InputProps) {
   return (
     <div className='flex items-start justify-center flex-col w-full'>
       <label htmlFor={name}>{label}</label>
@@ -24,7 +24,7 @@ export function Input({ id, name, type, className, placeholder, label, children,
             id={id}
             className={twMerge('peer w-full bg-[#D9D9D9] border-[#c8c8c8] border drop-shadow-md rounded-xl xs:text-xl text-lg h-12 pl-3 focus:outline-none focus:ring-1 focus:ring-slate-500 font-extralight xs:placeholder:text-lg placeholder:text-base placeholder:text-custom-gray', className)}
             placeholder={placeholder}
-            value={value ?? undefined}
+            defaultValue={defaultValue ?? undefined}
           />
           {children}
         </div>
@@ -35,7 +35,7 @@ export function Input({ id, name, type, className, placeholder, label, children,
           id={id}
           className={twMerge('w-full bg-[#D9D9D9] border-[#c8c8c8] border drop-shadow-md rounded-xl xs:text-xl text-lg h-12 pl-3 focus:outline-none focus:ring-1 focus:ring-slate-500 font-extralight xs:placeholder:text-lg placeholder:text-base placeholder:text-custom-gray', className)}
           placeholder={placeholder}
-          value={value ?? undefined}
+          value={defaultValue ?? undefined}
         />
       )}
     </div>
