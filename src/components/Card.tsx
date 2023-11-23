ab8b20151a038155f1180539a713d32ebcd09b30
Felipe <pipecalgaro@gmail.com>
2023-11-23 12:15:49 -0300

    fix: update home card availability display

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,3 @@
-import formatDate from '@/utils/formatDate'
 import formatPrice from '@/utils/formatPrice'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -6,19 +5,18 @@ import Link from 'next/link'
 interface CardProps {
   imageUrl: string
   location: [string, string]
-  availableFrom: Date
-  availableUntil: Date
+  isAvailable: boolean
   price: number
   id: string
 }
 
-export function Card({ availableFrom, availableUntil, imageUrl, location, price, id }: CardProps) {
+export function Card({ imageUrl, location, price, id, isAvailable }: CardProps) {
   return (
     <Link href={`homes/${id}`} className='cursor-pointer active:scale-[0.98] active:drop-shadow-sm transition-all duration-200 border-[1px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border-[rgba(102, 102, 102, 0.2)] bg-gradient-to-br from-gray-100 to-gray-50 drop-shadow-xl w-fit rounded-lg p-5 flex flex-col justify-center gap-y-6'>
       <Image src='/example.png' alt='House image' width={240} height={240} style={{ borderRadius: '8px' }} />
       <div className='flex flex-col gap-y-1 w-60'>
         <h1 className='font-semibold text-lg break-words'>{`${location[0]}, ${location[1]}`}</h1>
-        <p className='text-[#666666] text-[15px]'>{`${formatDate(availableFrom, 'pt', true)} - ${formatDate(availableUntil, 'pt', true)}`}</p>
+        <p className='text-[#666666] text-[15px]'>{isAvailable ? 'Disponível' : 'Não disponível'}</p>
         <p className='text-[#666666] text-[15px]'><span className='font-semibold text-black'>{formatPrice(price)}</span> por noite</p>
       </div>
     </Link>
