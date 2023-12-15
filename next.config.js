eb50f36391694eb0bcf9d585ce67380835a3156d
Felipe <pipecalgaro@gmail.com>
2023-12-15 14:16:43 -0300

    feat: configure images hostname

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
