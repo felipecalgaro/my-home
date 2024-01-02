705862f396f7d49659bcb0c68d5ff8faca8b373e
Felipe <pipecalgaro@gmail.com>
2024-01-02 20:16:10 -0300

    feat: add home description area

--- src/app/homes/[id]/page.tsx
+++ src/app/homes/[id]/page.tsx
@@ -1,6 +1,4 @@
 import { HomeDetail } from '@/components/HomeDetail'
-import { IconLabel } from '@/components/IconLabel'
-import { Input } from '@/components/Input'
 import { ReservationForm } from '@/components/ReservationForm'
 import { bookReservationService } from '@/services/bookReservationService'
 import { getHomeByIdService } from '@/services/getHomeByIdService'
@@ -55,23 +53,10 @@ export default async function Home({ params, searchParams }: HomeProps) {
       <section className='flex justify-center items-center w-full lg:gap-32 gap-20 flex-wrap'>
         <ReservationForm submitReservation={submitReservation} reservations={home.reservations} />
         <hr className='lg:h-80 h-px lg:w-px w-2/3 bg-[#d4d1d1]' />
-        <form className='flex flex-col justify-center items-start gap-12'>
-          <h1 className='text-4xl font-medium'>Contate o proprietário</h1>
-          <div className='flex flex-col justify-center items-start gap-2 w-full'>
-            <label className='ml-2 text-xl font-medium' htmlFor="email">Email</label>
-            <div className='flex justify-center items-center w-full'>
-              <IconLabel htmlFor='email'>
-                <Image src='/email.svg' alt='email-icon' width={24} height={24} />
-              </IconLabel>
-              <Input placeholder='Digite seu email' name='email' id='email' className='rounded-l-none rounded-r-3xl' />
-            </div>
-          </div>
-          <div className='flex flex-col justify-center items-start gap-2 w-full'>
-            <label className='ml-2 text-xl font-medium' htmlFor="message">Mensagem</label>
-            <textarea name='message' id='message' rows={8} placeholder='Digite a mensagem' className='bg-[#D9D9D9] focus:bg-[#e1e1e1] border-[#c8c8c8] border drop-shadow-sm rounded-3xl xs:text-xl text-lg p-4 xs:w-full w-64 focus:outline-none focus:ring-1 focus:ring-slate-500 font-extralight xs:placeholder:text-lg placeholder:text-base placeholder:text-custom-gray'></textarea>
-          </div>
-          <button className='hover:bg-custom-black border border-custom-black px-12 py-1.5 xs:text-lg text-base text-custom-black hover:text-white rounded-xl flex justify-center items-center gap-x-2 active:scale-95 transition-all duration-200'>Enviar</button>
-        </form>
+        <div className='flex flex-col justify-center items-start gap-12 w-1/4'>
+          <h1 className='text-4xl font-medium'>Descrição do local</h1>
+          <p className='break-words'>{home.description}</p>
+        </div>
       </section>
     </main>
   )
