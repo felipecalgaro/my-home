fc6c4c1c64e7bb2e668be71ac30ae31fa290fa3f
Felipe <pipecalgaro@gmail.com>
2024-01-04 18:55:42 -0300

    feat: add description textarea on form

--- src/app/homes/register/page.tsx
+++ src/app/homes/register/page.tsx
@@ -7,6 +7,7 @@ type RegisterFormData = {
   city: string
   state: string
   price: string
+  description: string
 }
 
 export default async function Register() {
@@ -17,7 +18,7 @@ export default async function Register() {
 
     if ([imageUrl, ...Object.values(data)].includes('')) return
 
-    await registerHomeService({ location: [data.city, data.state], ownerEmail: data.email, price: Number(data.price), image_url: imageUrl })
+    await registerHomeService({ location: [data.city, data.state], ownerEmail: data.email, price: Number(data.price), image_url: imageUrl, description: data.description })
   }
 
   return (
@@ -31,6 +32,10 @@ export default async function Register() {
         </div>
         <div className='flex justify-center items-end w-full'>
           <Input id='price' label='Preço da diária' name='price' step='10' placeholder='Digite o preço da diária (R$)' className='price-input' type='number' />
+        </div>
+        <div className='flex items-start justify-center flex-col w-full'>
+          <label htmlFor='description'>Descrição</label>
+          <textarea name='description' id='description' rows={8} placeholder='Descreva o local' className='bg-[#D9D9D9] focus:bg-[#e1e1e1] border-[#c8c8c8] border drop-shadow-sm rounded-xl xs:text-xl text-lg p-3 xs:w-full w-64 focus:outline-none focus:ring-1 focus:ring-slate-500 font-extralight xs:placeholder:text-lg placeholder:text-base placeholder:text-custom-gray'></textarea>
         </div>
         <ImageDropzone handleSubmit={handleSubmit} />
       </form>
