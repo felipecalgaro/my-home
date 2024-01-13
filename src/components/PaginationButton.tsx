b0a475aaa067e36c629af8a5257ba291190cb985
Felipe <pipecalgaro@gmail.com>
2024-01-13 20:06:35 -0300

    feat: create pagination feature

--- src/components/PaginationButton.tsx
+++ src/components/PaginationButton.tsx
@@ -1,11 +1,33 @@
+'use client'
+
 import Image from 'next/image'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 interface PaginationButtonProps {
   iconUrl: string
+  navigationType: 'NEXT' | 'PREVIOUS',
+  currentSkip: number
+  skip: number
 }
 
-export function PaginationButton({ iconUrl }: PaginationButtonProps) {
-  return <button className='rounded-full border border-custom-black bg-transparent p-4 aspect-square'>
-    <Image src={iconUrl} alt='Icon' width={32} height={32} />
-  </button>
+export function PaginationButton({ iconUrl, navigationType, skip, currentSkip }: PaginationButtonProps) {
+  const router = useRouter()
+  const params = useSearchParams()
+
+  const newParams = new URLSearchParams(params.toString())
+
+  const newSkip = navigationType === 'NEXT' ? String(currentSkip + skip) : String(currentSkip - skip)
+
+  return (
+    <button
+      onClick={(e) => {
+        e.preventDefault()
+        newParams.set('skip', newSkip)
+        router.push(`?${newParams.toString()}`, { scroll: false })
+      }}
+      className='rounded-full border border-custom-black bg-transparent p-4 aspect-square'
+    >
+      <Image src={iconUrl} alt='Icon' width={32} height={32} />
+    </button>
+  )
 }
